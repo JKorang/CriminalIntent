4777f586c5c32013128a3cfe0179f9b912b9bf58
Joseph Korang <jkorang@gmail.com>
2015-07-06 11:37:41 -0400

    Tutorial 2 Bugfix
    Because the tutorial was wrong.

--- app/src/main/java/edu/josephkorang/criminalintent/CrimeActivity.java
+++ app/src/main/java/edu/josephkorang/criminalintent/CrimeActivity.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 
-public class CrimeActivity extends FragmentActivity {
+public class CrimeActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
