9390c9b72f3e2a6c627e54868b8bbb1875b0755e
Joseph Korang <jkorang@gmail.com>
2015-07-08 00:23:18 -0400

    Quiz 1A

--- app/src/main/java/edu/josephkorang/criminalintent/CrimeFragment.java
+++ app/src/main/java/edu/josephkorang/criminalintent/CrimeFragment.java
@@ -7,11 +7,15 @@ import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class CrimeFragment extends Fragment {
-    Crime mCrime;
-    EditText mTitleField;
+    private Crime mCrime;
+    private EditText mTitleField;
+    private Button mDateButton;
+    private CheckBox mSolvedCheckBox;
 
     public CrimeFragment() {
 
@@ -25,6 +29,9 @@ public class CrimeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
         mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mDateButton = (Button) v.findViewById(R.id.crime_fragment_date_button);
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_list_item_solvedCheckBox);
+
         mTitleField.addTextChangedListener(new TextWatcher() {
             public void onTextChanged(CharSequence c, int start, int before, int count) {
                 mCrime.setTitle(c.toString());
@@ -36,6 +43,10 @@ public class CrimeFragment extends Fragment {
                 // this one too
             }
         });
+
+        //Set mDateButton to current date.
+        mDateButton.setText(mCrime.getDate().toString());
+        mDateButton.setEnabled(false);
         return v;
     }
 }
