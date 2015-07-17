60a3a86bcc7dc0c77afccb88fa010cede1a0fc93
Joseph Korang <jkorang@gmail.com>
2015-07-17 02:37:31 -0400

    Tutorial 3.1/3.2
    Implemented new features.  Had to bugfix due to Android 5.0+ igoring calendarViewShown.

--- app/src/main/java/edu/josephkorang/criminalintent/Crime.java
+++ app/src/main/java/edu/josephkorang/criminalintent/Crime.java
@@ -1,44 +1,76 @@
 package edu.josephkorang.criminalintent;
 
-import java.util.Date;
-import java.util.UUID;
+        import java.util.Date;
+        import java.util.UUID;
+
+        import org.json.JSONException;
+        import org.json.JSONObject;
+
 public class Crime {
+
+    private static final String JSON_ID = "id";
+    private static final String JSON_TITLE = "title";
+    private static final String JSON_DATE = "date";
+    private static final String JSON_SOLVED = "solved";
+
     private UUID mId;
     private String mTitle;
-    private boolean mSolved;
     private Date mDate;
+    private boolean mSolved;
+
     public Crime() {
         mId = UUID.randomUUID();
-        mSolved = false;
         mDate = new Date();
     }
-    public String getTitle() {
-        return mTitle;
+
+    public Crime(JSONObject json) throws JSONException {
+        mId = UUID.fromString(json.getString(JSON_ID));
+        mTitle = json.getString(JSON_TITLE);
+        mSolved = json.getBoolean(JSON_SOLVED);
+        mDate = new Date(json.getLong(JSON_DATE));
     }
-    public void setTitle(String title) {
-        mTitle = title;
+
+    public JSONObject toJSON() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put(JSON_ID, mId.toString());
+        json.put(JSON_TITLE, mTitle);
+        json.put(JSON_DATE, mDate.getTime());
+        json.put(JSON_SOLVED, mSolved);
+        return json;
     }
-    public UUID getId() {
-        return mId;
-    }
+
     @Override
     public String toString() {
         return mTitle;
     }
-    public void setSolved(boolean x) {
-        if (x == true) {
-            mSolved = true;
-        }
-        else {
-            mSolved = false;
-        }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public UUID getId() {
+        return mId;
     }
 
     public boolean isSolved() {
         return mSolved;
     }
 
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
     public Date getDate() {
         return mDate;
     }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+
 }
--- app/src/main/java/edu/josephkorang/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/edu/josephkorang/criminalintent/SingleFragmentActivity.java
@@ -1,25 +1,24 @@
 package edu.josephkorang.criminalintent;
 
 import android.os.Bundle;
+
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 
-/**
- * Created by root on 7/6/15.
- */
 public abstract class SingleFragmentActivity extends FragmentActivity {
     protected abstract Fragment createFragment();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+        setContentView(R.layout.activity_fragment);
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragmentContainer);
+
         if (fragment == null) {
             fragment = createFragment();
-            fm.beginTransaction()
+            manager.beginTransaction()
                     .add(R.id.fragmentContainer, fragment)
                     .commit();
         }
